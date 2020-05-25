2747e05b339a3e4135c8b5e85172ead4c666e9db
Nhan Nguyen <nhan.nguyen@wizeline.com>
2020-05-25 11:16:15 +0700

    Implement hazel cast

---
+++ src/main/java/com/example/demo/HazelCastConfiguration.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionPolicy;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.MaxSizeConfig;
+import com.hazelcast.config.NetworkConfig;
+
+@Configuration
+public class HazelCastConfiguration {
+	@Bean
+    public Config hazelCastConfig(){
+        Config config = new Config();
+        config.setInstanceName("hazelcast-instance")        // hazel case instance name
+                .addMapConfig(
+                        new MapConfig()                     // create map
+                                .setName("configuration")
+                                .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+                                .setEvictionPolicy(EvictionPolicy.LRU)
+                                .setTimeToLiveSeconds(-1));     // cache will be available until it will remove manually. less then 0 means never expired.
+        
+        //network config
+        NetworkConfig network = config.getNetworkConfig();
+
+        JoinConfig join = network.getJoin();
+        join.getTcpIpConfig().setEnabled(false);
+        join.getAwsConfig().setEnabled(false);
+        join.getMulticastConfig().setEnabled(true);
+        return config;
+    }
+}
--- src/main/java/com/example/demo/course/CourseDetailService.java
+++ src/main/java/com/example/demo/course/CourseDetailService.java
@@ -1,7 +1,12 @@
 package com.example.demo.course;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import com.hazelcast.core.HazelcastInstance;
 
 import net.javacrumbs.shedlock.core.SchedulerLock;
 
@@ -9,10 +14,15 @@ import net.javacrumbs.shedlock.core.SchedulerLock;
 public class CourseDetailService {
 	private static int count = 0;
 	
+	@Autowired
+	private HazelcastInstance hazelCast;
+	
 	@Scheduled(fixedRate = 5000)
 	@SchedulerLock(name = "TaskScheduler_scheduledTask", 
     lockAtLeastForString = "PT5S", lockAtMostForString = "PT10S")
 	public void printCourse() {
-		System.out.println("I'm " + ++count);
+		Map<String, Integer> mapValue = hazelCast.getMap("configuration");
+		mapValue.put("data", ++count);
+		System.out.println("I'm " + mapValue.get("data"));
 	}
 }
